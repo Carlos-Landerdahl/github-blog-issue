c32685693a0290ff5f6330128baea083dfda5d8d
charlim <carloslanderdahl27@gmail.com>
2023-01-26 14:04:11 -0300

    feat: setting global styles with styled-components

---
+++ src/styles/global.ts
@@ -0,0 +1,49 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *{ 
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+      outline: 0;
+      box-shadow: 0 0 0 2px ${(props) => props.theme.blue};
+    }
+
+  body {
+    background-color: ${(props) => props.theme['base-background']};
+    color: ${(props) => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+    }
+
+  body, input, textarea, button {
+    font: 400 1rem 'Nunito', sans-serif;
+  }
+`
+
+export const Title = styled.h1`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 130%;
+  color: ${(props) => props.theme['base-title']};
+`
+
+export const Subtitle = styled.h2`
+  font-family: 'Nunito';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 160%;
+  color: ${(props) => props.theme['base-text']};
+`
+
+export const SmallPrint = styled.p`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 130%;
+  color: ${(props) => props.theme['base-span']};
+`
